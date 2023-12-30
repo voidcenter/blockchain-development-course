4ca35b39dc8f36a515866295f8ecd3a5f5650a7e
Justin <justin@Justins-MacBook-Pro.local>
2023-12-30 13:11:32 -0800

    contract sizer

--- MyERC20/hardhat.config.ts
+++ MyERC20/hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-contract-sizer";
 require('dotenv').config()
 
 const { ALCHEMY_SEPOLIA_KEY, ACCOUNT_PRIVATE_KEY } = process.env;
@@ -12,7 +13,14 @@ const config: HardhatUserConfig = {
       url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_SEPOLIA_KEY}`,
       accounts: [ACCOUNT_PRIVATE_KEY]
     }
-  }
+  },
+
+  contractSizer: {
+    alphaSort: true,
+    disambiguatePaths: false,
+    runOnCompile: true,
+    strict: true,
+  },
 };
 
 export default config;

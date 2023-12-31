8523d4b7dddf9581fc12b20ee92a092679df57d8
Justin <justin@Justins-MacBook-Pro.local>
2023-12-30 17:23:11 -0800

    refactor

--- MyERC20/frontend/pages/_app.tsx
+++ MyERC20/frontend/pages/_app.tsx
@@ -1,12 +1,15 @@
 import './globals.css';
 import { AppProps } from 'next/app';
 import dynamic from 'next/dynamic';
+import { Web3Modal } from "../context/Web3Modal";
 
 
 function App({ Component, pageProps }: AppProps) {
     return (
-        <Component {...pageProps} />
-    );
+        <Web3Modal>
+            <Component {...pageProps} />
+        </Web3Modal>
+   );
 }
 
 export default dynamic(() => Promise.resolve(App), {
--- MyERC20/frontend/pages/index.tsx
+++ MyERC20/frontend/pages/index.tsx
@@ -1,8 +1,34 @@
+
+import { useEffect } from "react";
+import { readContract, writeContract } from "wagmi/actions";
+const mud_abi = require('../../common/GameSystem.abi.json');
+
+const MyERC20TokenAddress = "0x5ae8b20195d12da6A5F1ae5d9fFD775464E952bc";
+
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-    </main>
-  )
+
+    useEffect(() => {
+        const run = async () => {
+
+            await readContract({
+                address: MyERC20TokenAddress,
+                abi: this.mud_abi,
+                functionName: name,
+                args: [],
+            });
+
+        };
+
+        run();
+    });
+
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between">
+            <div className="fixed top-0 right-0 rounded-full">   
+                <w3m-button />
+            </div>
+        </main>
+    )
 }
 
 

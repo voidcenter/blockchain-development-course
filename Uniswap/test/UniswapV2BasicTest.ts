67a3471dc4334f899af97a0ff0090c089c145962
Justin <justin@Justins-MacBook-Pro.local>
2024-01-14 00:26:16 -0800

    uncomment test

--- Uniswap/test/UniswapV2BasicTest.ts
+++ Uniswap/test/UniswapV2BasicTest.ts
@@ -1,23 +1,23 @@
-// import { deployBasicTestContracts, basicTest, printBasicTestContracts, flashloanTest } from "./common/basicTest";
-// import { getSigners } from "./common/common";
+import { deployBasicTestContracts, basicTest, printBasicTestContracts, flashloanTest } from "./common/basicTest";
+import { getSigners } from "./common/common";
 
 
-// describe("E2E Test", function () {
+describe("E2E Test", function () {
 
-//     it("Basic test should succeed", async function () {
-//         const signers = await getSigners();
-//         // console.log('owner address = ', signers.owner.address)
-//         // console.log('swapper address = ', signers.swapper!.address)
+    it("Basic test should succeed", async function () {
+        const signers = await getSigners();
+        // console.log('owner address = ', signers.owner.address)
+        // console.log('swapper address = ', signers.swapper!.address)
     
-//         const contracts = await deployBasicTestContracts(signers, true);
-//         printBasicTestContracts(contracts);
-//         await basicTest(signers, contracts, true);
-//     });
+        const contracts = await deployBasicTestContracts(signers, true);
+        printBasicTestContracts(contracts);
+        await basicTest(signers, contracts, true);
+    });
 
-//     it("Flashloan test should succeed", async function () {
-//         const signers = await getSigners();
-//         const contracts = await deployBasicTestContracts(signers, true);
-//         printBasicTestContracts(contracts);
-//         await flashloanTest(signers, contracts, true);
-//     });
-// });
+    it("Flashloan test should succeed", async function () {
+        const signers = await getSigners();
+        const contracts = await deployBasicTestContracts(signers, true);
+        printBasicTestContracts(contracts);
+        await flashloanTest(signers, contracts, true);
+    });
+});

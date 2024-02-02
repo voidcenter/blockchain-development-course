1b4114adf3cac95d775c4bd4bdbb2d0a968d7372
Justin <justin@Justins-MacBook-Pro.local>
2024-02-02 10:34:52 +0800

    test

--- UniswapV2/contracts/UniswapV2Pair.sol
+++ UniswapV2/contracts/UniswapV2Pair.sol
@@ -148,7 +148,6 @@ contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
     function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external lock {
         require(amount0Out > 0 || amount1Out > 0, 'UniswapV2: INSUFFICIENT_OUTPUT_AMOUNT');
         (uint112 _reserve0, uint112 _reserve1) = getReserves(); 
-
         require(amount0Out < _reserve0 && amount1Out < _reserve1, 'UniswapV2: INSUFFICIENT_LIQUIDITY');
 
         uint balance0;
@@ -162,10 +161,8 @@ contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
             if (amount0Out > 0) _safeTransfer(_token0, to, amount0Out); 
             if (amount1Out > 0) _safeTransfer(_token1, to, amount1Out); 
 
-            // call callback, this is only useful for flashloan
             if (data.length > 0) IUniswapV2Callee(to).uniswapV2Call (msg.sender, amount0Out, amount1Out, data);
 
-            // token balances after the swap
             balance0 = IERC20(_token0).balanceOf(address(this));
             balance1 = IERC20(_token1).balanceOf(address(this));
         }
@@ -177,10 +174,7 @@ contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
         { 
             uint balance0Adjusted = balance0.mul(1000).sub(amount0In.mul(3));
             uint balance1Adjusted = balance1.mul(1000).sub(amount1In.mul(3));
-
-            require(   balance0Adjusted.mul(balance1Adjusted) 
-                    >= uint(_reserve0).mul(_reserve1).mul(1000**2), 
-                    'UniswapV2: K');
+            require(balance0Adjusted.mul(balance1Adjusted) >= uint(_reserve0).mul(_reserve1).mul(1000**2), 'UniswapV2: K');
         }
 
         _update(balance0, balance1);
